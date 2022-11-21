bfa2cea4aa3bf9c5bd41ef88e6a5cff28b5e0200
Shawn Ogbomo <44599293+Shawn-Ogbomo@users.noreply.github.com>
2022-11-21 14:46:26 -0500

    Working on library type and Transaction

--- library.h
+++ library.h
@@ -1,20 +1,20 @@
 #pragma once
-#include <string>
-class Patron
+#include <vector>
+#include "book.h"
+#include "patron.h"
+#include "date.h"
+struct Transaction
 {
-	static constexpr int first_name_max = 12;
-	static constexpr int last_name_max = 12;
+	Book book;
+	Patron member;
+	Date_lib::Date date;
+	std::vector<Transaction> transactions;
+};
+class Library
+{
 public:
-	Patron(std::string member_name, std::string member_card_number);
-	std::string get_name() const;
-	std::string get_card_number() const;
-	double get_fees() const;
 
 private:
-	std::string name;
-	std::string card_number;
-	double fees;
-	bool in_debt;
-	bool is_valid();
+	std::vector<Book> books;
+	std::vector<Patron> members;
 };
-bool debt(const Patron& member);
