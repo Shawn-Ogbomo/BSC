f65dace0600382a80c0f8431450c30c0b57c06a7
Shawn Ogbomo <44599293+Shawn-Ogbomo@users.noreply.github.com>
2022-12-05 11:53:48 -0500

    working on operator+

--- rational.cpp
+++ rational.cpp
@@ -1,1 +1,27 @@
 #include "rational.h"
+#include  <stdexcept>
+Rational_number::Rational_number(int n, int d)
+	: numerator{ n },
+	denominator{ d },
+	quotient{}{
+	if ((numerator == 0) || (numerator < 0 && denominator == 0)) {
+		throw std::runtime_error("Cannot divide by 0...");
+	}
+	quotient = static_cast<double>(n) / d;
+}
+
+int Rational_number::get_numerator() const {
+	return numerator;
+}
+
+int Rational_number::get_denominator() const {
+	return denominator;
+}
+
+double Rational_number::get_quotient() const {
+	return quotient;
+}
+
+Rational_number operator+(const Rational_number& lhs, const Rational_number& rhs) {
+	double result = lhs.get_quotient() + rhs.get_quotient();
+}
--- rational.h
+++ rational.h
@@ -2,6 +2,13 @@
 class Rational_number
 {
 public:
-
+	Rational_number(int n, int d);
+	int get_numerator() const;
+	int get_denominator() const;
+	double get_quotient() const;
 private:
+	int numerator;
+	int denominator;
+	double quotient;
 };
+Rational_number operator +(const Rational_number& lhs, const Rational_number& rhs);
