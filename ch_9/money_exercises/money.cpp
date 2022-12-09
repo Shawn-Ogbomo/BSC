56ffc0c48a006139d19692f5b08c3a32747f23bf
Shawn Ogbomo <44599293+Shawn-Ogbomo@users.noreply.github.com>
2022-12-09 17:41:44 -0500

    working on operator* () for Exchange rate struct

--- ch_9/money_exercises/money.cpp
+++ ch_9/money_exercises/money.cpp
@@ -13,6 +13,10 @@ Money::Money(std::string currency_code, double qty)
 	: total_cents{ (static_cast<int>(qty) * 100) },
 	code{ currency_code } {
 	static const std::vector<std::string> valid_currenicies = { "USD","CAD","JPY","NGN", "DKK","CNY" };
+	if (currency_code.size() > max_currency_size) {
+		std::cerr << "Exceeded character limit...\nCurrencies are 3 letters long...\n";
+		throw Invalid{};
+	}
 	for (auto i = 0; i < code.size(); ++i) {
 		code[i] = std::toupper(code[i]);
 	}
@@ -22,7 +26,7 @@ Money::Money(std::string currency_code, double qty)
 		throw Invalid{};
 	}
 	if (qty <= 0) {
-		std::cerr << "Money cannot be zero or a negative value...";
+		std::cerr << "Money cannot be zero or a negative value...\n";
 		throw Invalid{};
 	}
 	total_cents += std::round((qty - static_cast<int>(qty)) * 100);
@@ -34,15 +38,20 @@ Money::Money(std::string currency_code, double qty)
 std::string Money::currency() const {
 	return code;
 }
+long Money::amount() const {
+	return total_cents;
+}
 Money::operator double() const {
 	return (total_cents / static_cast<double>(100));
 }
 std::istream& operator>>(std::istream& is, Money& m) {
 	double amount = 0;
-	char symbol = ' ';
+	char symbol{};
 	std::string code;
 	is >> symbol >> code >> amount;
-	if (!is) { return is; }
+	if (!is) {
+		return is;
+	}
 	if (symbol != '$') {
 		is.clear(std::ios_base::failbit);
 		return is;
@@ -51,13 +60,30 @@ std::istream& operator>>(std::istream& is, Money& m) {
 	return is;
 }
 std::ostream& operator<<(std::ostream& os, const Money& m) {
-	return os << '$ ' << static_cast<double>(m);
+	return os << "$" << static_cast<double>(m);
 }
 Money operator+(const Money& left, const Money& right) {
 	if (left.currency() != right.currency()) {
+		return exchange(left, right);
+	}
+	return Money{ left.currency(), static_cast<double>(left) + static_cast<double>(right) };
+}
+Money exchange(const Money& left, const Money& right) {
+	if (left.currency() == "USD") {
+		Echange_rates::USD american_exchange;
+		return Money{ left.currency(), left.operator double() + american_exchange.operator*(right) };
 	}
 }
-
 //Money operator-(const Money& left, const Money& right) {
 //	return static_cast<double>(left) - static_cast<double>(right);
 //}
+double Echange_rates::USD::operator*(const Money& m) {
+	if (m.currency() == "CAD") {
+		std::cout << m.operator double() * cad_to_usd;
+		return m.operator double() * cad_to_usd;
+	}
+	else if (m.currency() == "JPY") {}
+	else if (m.currency() == "NGN") {}
+	else if (m.currency() == "DKK") {}
+	else if (m.currency() == "CNY") {}
+}
--- ch_9/money_exercises/money.h
+++ ch_9/money_exercises/money.h
@@ -1,16 +1,70 @@
 #pragma once
 class Money {
+	static constexpr int max_currency_size = 3;
 public:
 	Money();
 	Money(std::string currency_code, double qty);
 	std::string currency() const;
+	long amount() const;
 	explicit operator double() const;
 	class Invalid {};
 private:
 	std::string code;
-	long int total_cents;
+	long total_cents;
 };
 std::istream& operator >>(std::istream& is, Money& m);
 std::ostream& operator <<(std::ostream& os, const Money& m);
 Money operator +(const Money& left, const Money& right);
+Money exchange(const Money& left, const Money& right);
 //Money operator -(const Money& left, const Money& right);
+struct Echange_rates {
+	struct USD {
+		static constexpr double cad_to_usd = 0.73;
+		static constexpr double jpy = 136.47;
+		static constexpr double ngn = 445.02;
+		static constexpr double dkk = 7.06;
+		static constexpr double cny = 6.96;
+		double operator*(const Money& m);
+	};
+};
+/*struct CAD {
+	static constexpr double to_usd = 0.74;
+	static constexpr double to_jpy = 100.21;
+	static constexpr double to_ngn = 326.79;
+	static constexpr double to_dkk = 5.18;
+	static constexpr double to_cny = 5.11;
+	double operator*(const std::string& c);
+};
+struct JPY {
+	static constexpr double to_usd = 0.0073;
+	static constexpr double to_cad = 0.0100;
+	static constexpr double to_ngn = 3.26;
+	static constexpr double to_dkk = 0.052;
+	static constexpr double to_cny = 0.051;
+	double operator*(const std::string& c);
+};
+struct NGN {
+	static constexpr double to_usd = 0.0022;
+	static constexpr double to_cad = 0.0031;
+	static constexpr double to_jpy = 0.31;
+	static constexpr double to_dkk = 0.016;
+	static constexpr double to_cny = 0.016;
+	double operator*(const std::string& c);
+};
+struct DKK {
+	static constexpr double to_usd = 0.14;
+	static constexpr double to_cad = 0.19;
+	static constexpr double to_jpy = 19.35;
+	static constexpr double to_ngn = 63.10;
+	static constexpr double to_cny = 1.36;
+	double operator*(const std::string& c);
+};
+struct CNY {
+	static constexpr double to_usd = 0.14;
+	static constexpr double to_cad = 0.20;
+	static constexpr double to_jpy = 19.62;
+	static constexpr double to_ngn = 63.96;
+	static constexpr double to_dkk = 1.01;
+	double operator*(const std::string& c);
+};*/
+//};
