07c79c2c5eef80076a3f795910955b643b612b95
Shawn Ogbomo <Shawn_Ogbomo@hotmail.com>
2022-12-22 13:53:36 -0500

    working on istream>>

--- ch_10/ch_10/point.cpp
+++ ch_10/ch_10/point.cpp
@@ -1,4 +1,11 @@
+#include <iostream>
 #include "point.h"
+int Point::x_cord() const {
+	return x;
+}
+int Point::y_cord() const {
+	return y;
+}
 Point::Point()
 	: x{},
 	y{} {
@@ -7,4 +14,20 @@ Point::Point()
 Point::Point(int coord_x, int coord_y)
 	: x{ coord_x },
 	y{ coord_y } {
+}
+
+std::istream& operator>>(std::istream& is, Point& p) {
+	int val1{};
+	int val2{};
+	is >> val1 >> val2;
+	if (!is) {
+		return is;
+	}
+	p = Point{ val1,val2 };
+	return is;
+}
+
+std::ostream& operator<<(std::ostream& os, const Point& p) {
+	os << p.x_cord() << " " << p.y_cord();
+	return os;
 }
--- ch_10/ch_10/point.h
+++ ch_10/ch_10/point.h
@@ -1,9 +1,14 @@
 #pragma once
 class Point {
 public:
+	class Invalid {};
+	int x_cord() const;
+	int y_cord() const;
 	Point();
 	Point(int coord_x, int coord_y);
 private:
 	int x;
 	int y;
 };
+std::istream& operator>>(std::istream& is, Point& p);
+std::ostream& operator<<(std::ostream& os, const Point& p);
