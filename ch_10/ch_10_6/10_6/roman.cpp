8e144af44c19c958994b929a3322b0d1d7055b15
Shawn Ogbomo <Shawn_Ogbomo@hotmail.com>
2022-12-29 18:59:59 -0500

    working on 1 arg ctor

--- ch_10/ch_10_6/10_6/roman.cpp
+++ ch_10/ch_10_6/10_6/roman.cpp
@@ -1,1 +1,42 @@
+#include <iostream>
+#include <cctype>
 #include "roman.h"
+Roman_int::Roman_int()
+	:roman_code{ "nulla" },
+	value{}{
+}
+Roman_int::Roman_int(const std::string& symbols)
+	: roman_code{ symbols },
+	value{}{
+	if (symbols.size() > max_sz) {
+		std::cerr << "Too many symbols...\n";
+		throw Invalid{};
+	}
+	for (auto target : symbols) {
+		if (islower(target)) {
+			target = toupper(target);
+		}
+		switch (target) {
+		case'I':
+
+		case'V':
+
+		case'X':
+
+		case'L':
+
+		case'C':
+
+		case'D':
+
+		case'M':
+		default:
+			std::cerr << "Invalid Roman symbol...\n";
+			throw Invalid{};
+			break;
+		}
+	}
+}
+int Roman_int::as_int() const {
+	return value;
+}
--- ch_10/ch_10_6/10_6/roman.h
+++ ch_10/ch_10_6/10_6/roman.h
@@ -1,7 +1,13 @@
 #pragma once
-class Roman_int
-{
+#include <string>
+class Roman_int {
+	static const int max_sz = 4;
 public:
+	class Invalid {};
 	Roman_int();
+	Roman_int(const std::string& symbols);
+	int as_int() const;
 private:
+	std::string roman_code;
+	int value;
 };
