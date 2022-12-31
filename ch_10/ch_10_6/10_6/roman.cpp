5f6068bdf37b2cf5c50d2672c347c6bd1c3e99e6
Shawn Ogbomo <Shawn_Ogbomo@hotmail.com>
2022-12-31 12:03:05 -0500

    working on roman_int()

--- ch_10/ch_10_6/10_6/roman.cpp
+++ ch_10/ch_10_6/10_6/roman.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 #include <cctype>
 #include "roman.h"
+#include "util.h"
 Roman_int::Roman_int()
 	:roman_code{ "nulla" },
 	value{}{
@@ -12,31 +15,39 @@ Roman_int::Roman_int(const std::string& symbols)
 		std::cerr << "Too many symbols...\n";
 		throw Invalid{};
 	}
-	for (auto target : symbols) {
-		if (islower(target)) {
-			target = toupper(target);
+	std::vector<char>valids = { 'I','V','X','L','C','D','M' };
+	for (int i = 0; i < roman_code.size(); ++i) {
+		if (islower(symbols[i])) {
+			roman_code[i] = toupper(symbols[i]);
 		}
-		switch (target) {
-		case'I':
-
-		case'V':
-
-		case'X':
-
-		case'L':
-
-		case'C':
-
-		case'D':
-
-		case'M':
-		default:
-			std::cerr << "Invalid Roman symbol...\n";
+		if (auto search = std::find(std::begin(valids), std::end(valids), roman_code[i]) == valids.end()) {
+			std::cerr << "Invalid Roman symbol: " << roman_code[i] << "\n";
 			throw Invalid{};
-			break;
+		}
+	}
+
+	for (int i = 0, count = 1; i < roman_code.size(); ++i) {
+		for (int j = i + 1; j < roman_code.size(); ++j) {
+			if (roman_code[j] == roman_code[i]) {
+				++count;
+				if (count == repeat_limit) {
+					std::cerr << "Roman numerals can only repeat 3 times consecutively...\n";
+					throw Invalid{};
+				}
+			}
+			else if (roman_code[j] != roman_code[i]) {
+				count = 0;
+			}
 		}
 	}
 }
 int Roman_int::as_int() const {
 	return value;
+}
+
+std::string Roman_int::as_symbols() const {
+	return roman_code;
+}
+std::ostream& operator<<(std::ostream& os, const Roman_int& r) {
+	return os << r.as_symbols();
 }
--- ch_10/ch_10_6/10_6/roman.h
+++ ch_10/ch_10_6/10_6/roman.h
@@ -1,13 +1,17 @@
 #pragma once
 #include <string>
 class Roman_int {
-	static const int max_sz = 4;
+	static constexpr int max_sz = 9;
+	static constexpr int repeat_limit = 4;
 public:
 	class Invalid {};
 	Roman_int();
 	Roman_int(const std::string& symbols);
 	int as_int() const;
+	std::string as_symbols() const;
 private:
 	std::string roman_code;
 	int value;
 };
+std::ostream& operator << (std::ostream& os, const Roman_int& r);
+//std::istream& operator >> (std::istream& is, Roman_int& r);
