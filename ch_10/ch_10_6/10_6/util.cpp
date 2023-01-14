fd216ce6ee0c604afc27efa4b58e0363235be4df
Shawn Ogbomo <Shawn_Ogbomo@hotmail.com>
2023-01-13 22:51:34 -0500

    added prev_val()

--- ch_10/ch_10_6/10_6/util.cpp
+++ ch_10/ch_10_6/10_6/util.cpp
@@ -8,6 +8,13 @@ bool Util::next_value(const std::string& target, const int index) {
 	return false;
 }
 
+bool Util::previous_value(const std::string& target, const int index) {
+	if (target[index]) {
+		return true;
+	}
+	return false;
+}
+
 int Util::find_duplicates(const std::string& target_string, char c) {
 	if (target_string.size()) {
 		const int num_items = std::count(target_string.begin(), target_string.end(), c);
