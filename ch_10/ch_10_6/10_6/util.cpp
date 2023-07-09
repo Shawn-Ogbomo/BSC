3d393110ad419f88c1a52e3355c16f1b4d8b2eb8
Shawn Ogbomo <Shawn_Ogbomo@hotmail.com>
2023-07-09 13:14:58 -0400

    update

--- ch_10/ch_10_6/10_6/util.cpp
+++ ch_10/ch_10_6/10_6/util.cpp
@@ -1,44 +1,16 @@
+#include <map>
 #include <string_view>
 #include <algorithm>
 #include <iostream>
 #include <cctype>
 #include "util.h"
 
-bool Util::next_value(const std::string& target, const int index) {
-	if (target[index]) {
-		return true;
-	}
-	return false;
-}
-
-bool Util::previous_value(const int index) {
-	if (index != std::string::npos) {
-		return true;
-	}
-	return false;
-}
-
 int Util::find_duplicates(std::string_view target_string, char c) {
 	if (target_string.size()) {
 		const int num_items = std::count(target_string.begin(), target_string.end(), c);
 		return num_items;
 	}
-	throw std::length_error("oops, the string is empty...\n");
-}
-
-bool Util::duplicate_substrs(std::string_view target_string, std::string_view sub_str) {
-	if (sub_str.empty()) {
-		throw std::length_error("oops, you passed in an empty string \n");
-	}
-
-	if (int pos = target_string.find(sub_str); pos != std::string::npos) {
-		if (int pos2 = target_string.find(sub_str, (pos + sub_str.size()))) {
-			if (pos2 != std::string::npos) {
-				return true;
-			}
-		}
-	}
-	return false;
+	throw std::invalid_argument("oops, the string is empty...\n");
 }
 
 bool Util::repeats(const std::string& target_string, char c, unsigned pos) {
@@ -59,5 +31,5 @@ bool Util::repeats(const std::string& target_string, char c, unsigned pos) {
 		}
 		return false;
 	}
-	throw std::length_error("oops, the string is empty...\n");
+	throw std::invalid_argument("oops, the string is empty...\n");
 }
--- ch_10/ch_10_6/10_6/util.h
+++ ch_10/ch_10_6/10_6/util.h
@@ -1,9 +1,23 @@
 #pragma once
+#include <string_view>
+#include <functional>
 #include <string>
+#include <map>
+
 namespace Util {
-	bool next_value(const std::string& target, const int index);
-	bool previous_value(const int index);
 	int find_duplicates(std::string_view target_string, char c);
-	bool duplicate_substrs(std::string_view target_string, std::string_view sub_str);
 	bool repeats(const std::string& target_string, char c, unsigned pos);	//returns true if c repeats more than 3 times in succession...
+	inline auto lookup(const std::map <std::string, int, std::less<>>& m, std::string_view rmn_code, int current_index, int new_index) {
+		if (new_index && (new_index < rmn_code.size())) {
+			return m.find(rmn_code[current_index] + std::string{ rmn_code[new_index] });
+		}
+		return m.end();
+	}
+
+	template <typename T>
+	void increment(const std::map <std::string, int, std::less<>>& m, const T it, unsigned& index) {
+		if (it != m.end()) {
+			++index;
+		}
+	}
 }
