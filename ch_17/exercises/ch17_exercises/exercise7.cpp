bf10e63cbdd7dab75171a1601827eaef75aaafe3
Shawn Ogbomo <Shawn_Ogbomo@hotmail.com>
2024-06-05 02:26:29 -0400

    -edit exercise 7

--- ch_17/exercises/ch17_exercises/exercise7.cpp
+++ ch_17/exercises/ch17_exercises/exercise7.cpp
@@ -4,19 +4,42 @@
 //exhaustion.
 
 #include <iostream>
-
+//hold max size
 int main() {
 	std::cout << "Please enter some characters. Press ctrl+z or ! to exit...\n";
 
-	for (auto c = '?'; std::cin >> c;)
+	auto* p1 = new char[6] {};
+	p1[5] = '\0';
+
+	auto i{ 0 };
+	auto max_sz{ 5 };
+
+	for (auto c = '?'; std::cin.get(c);)
 	{
 		if (c == '!')
 		{
 			std::cout << "Exiting...";
+			delete[] p1;
 			return 0;
 		}
 
-		auto* p1 = new char[1];
-		p1[0] = c;
+		if (i < max_sz)
+		{
+			p1[i++] = c;
+			continue;
+		}
+
+		max_sz *= 2;
+		auto* p2 = new char[max_sz + 1] {};
+
+		for (auto internal_i = 0; internal_i < i; ++internal_i)
+		{
+			p2[internal_i] = p1[internal_i];
+		}
+
+		p2[max_sz] = '\0';
+		delete[] p1;
+		p1 = p2;
+		p1[i++] = c;
 	}
 }
