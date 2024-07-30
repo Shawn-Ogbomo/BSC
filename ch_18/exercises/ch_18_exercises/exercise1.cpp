8d58093634976702321ac607c3cca1af06c384d2
Shawn Ogbomo <Shawn_Ogbomo@hotmail.com>
2024-07-30 19:56:59 -0400

    -Finished exercise 1

---
+++ ch_18/exercises/ch_18_exercises/exercise1.cpp
@@ -0,0 +1,57 @@
+//“1. Write a function, char* strdupp(const char*),
+//that copies a C-style string into memory it allocates on the free store.
+//Do not use any standard library functions.
+//Do not use subscripting; use the dereference operator * instead.”
+#include <iostream>
+
+static auto strdupp(const char* target) -> char* {
+	if (!target) {
+		return nullptr;
+	}
+
+	auto* p = target;
+
+	auto target_sz = 0;
+
+	while (*p != '\0') {
+		++p;
+		++target_sz;
+	}
+
+	auto* str_cpy = new char[target_sz + 1] {};
+
+	auto* p2 = str_cpy;
+
+	auto index = 0;
+
+	while (index < target_sz)
+	{
+		*p2 = (*target);
+		++p2;
+		++target;
+		++index;
+	}
+
+	return str_cpy;
+}
+
+int main() {
+	char name[]{ "shawn" };
+
+	auto* p = strdupp(name);
+
+	std::cout << "Address of p: " << p << "\n";
+	std::cout << "Address of name: " << &name << "\n";
+
+	if (p)
+	{
+		const auto* p2 = p;
+
+		while (*p2 != '\0') {
+			std::cout << *p2;
+			++p2;
+		}
+	}
+
+	delete[] p;
+}
